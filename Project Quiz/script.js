dae0a2b60ab10499e3a51a2b512e9b287635f904
Rnveer007 <rvs523474@gmail.com>
2024-11-15 13:05:54 +0530

    Project

--- Project Quiz/script.js
+++ Project Quiz/script.js
@@ -8,70 +8,68 @@ let timer;
 let interval;
 let timerValue = 5;
 let optionSelected;
-// userclick = [];              
+userclick = [];              
 
-// function random() {
-//     return Math.floor(Math.random() * questions.length);
-// }
+function random() {
+    return Math.floor(Math.random() * questions.length);
+}
 
 btn_start.addEventListener("click", function () {
     quiz_storage.style.display = "block";
-    // displayQuestion();
+    displayQuestion();
 
-    // interval = setInterval(() => {
-    //     if (timerValue < 1) {
-    //         timerValue = 5;
-    //         displayQuestion()
-    //     }
-    //     else {
-    //         timerValue--;
-    //         timer.innerHTML = timerValue;
+    interval = setInterval(() => {
+        if (timerValue < 1) {
+            timerValue = 5;
+            displayQuestion()
+        }
+        else {
+            timerValue--;
+            timer.innerHTML = timerValue;
 
 
-    //     }
-    // },1000)
+        }
+    },1000)
     btn_start.style.display = "none"
 
 })
 
-// function displayQuestion() {
-//     playQuiz.innerHTML = ""
-//     let randomQuistions = questions[random()];
-//     let question = document.createElement("h1");
-//     question.innerHTML = randomQuistions.q;
+function displayQuestion() {
+    playQuiz.innerHTML = ""
+    let randomQuistions = questions[random()];
+    let question = document.createElement("h1");
+    question.innerHTML = randomQuistions.q;
      
-//     timer.innerHTML = timerValue;
+    timer.innerHTML = timerValue;
  
-//     playQuiz.append(question,timer);
+    playQuiz.append(question,timer);
 
-//     let options = randomQuistions.opt;
-//     let optiondiv = document.createElement("div")
+    let options = randomQuistions.opt;
+    let optiondiv = document.createElement("div")
 
-    
-
-    // options.forEach((option) => {
-    //     let btn = document.createElement("button");
-    //     btn.innerHTML = options 
-    //     optiondiv.append(option)
+    options.forEach((option) => {
+        let btn = document.createElement("button");
+        btn.innerHTML = options 
+        optiondiv.append(option)
         
-    // });
-    // playQuiz.append(optiondiv)
+    });
+    playQuiz.append(optiondiv)
 
-    // optiondiv.addEventListener("click",function(e){
-    //   if(e.target.tagName="BUTTON"){
-    //     optiondiv.querySelectorAll("button").forEach(btn=>btn.disabled=true)
-    //     e.target.disabled=false
-    //     userclick.push(e.target.innerHTML)
-    //     optionSelected=true
-    //   }
+    optiondiv.addEventListener("click",function(e){
+      if(e.target.tagName="BUTTON"){
+        optiondiv.querySelectorAll("button").forEach(btn=>btn.disabled=true)
+        e.target.disabled=false
+        userclick.push(e.target.innerHTML)
+        optionSelected=true
+      }
 
-    // })
+    })
 
-        // options.forEach(btn => btn.disable=true);
+        options.forEach(btn => btn.disable=true);
 
     
 
-// }
+}
 
 
 
